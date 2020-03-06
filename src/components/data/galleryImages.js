b9e98da8da6da52bc5e01ee2960fc52d3422f0df
Kamil Sobczyk <kamil.sobczyk@cobrick.com>
2020-03-06 17:51:53 +0100

    add new images

--- src/components/data/galleryImages.js
+++ src/components/data/galleryImages.js
@@ -1,148 +1,180 @@
 const images = [
   {
-    original: require("../../assets/gallery/g1-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g1-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g2-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g2-min-min.jpg'),
+    title: 'picture'
+  },
+  // {
+  //   original: require('../../assets/gallery/g3-min-min.jpg'),
+  //   title: 'picture'
+  // },
+  {
+    original: require('../../assets/gallery/g4-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g3-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g5-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g4-min-min.jpg"),
-    title: "picture"
-  },
-  {
-    original: require("../../assets/gallery/g5-min-min.jpg"),
-    title: "picture"
-  },
-  {
-    original: require("../../assets/gallery/g6-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g6-min-min.jpg'),
+    title: 'picture'
   },
 
   {
-    original: require("../../assets/gallery/g10-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g10-min-min.jpg'),
+    title: 'picture'
+  },
+  // {
+  //   original: require('../../assets/gallery/g11-min-min.jpg'),
+  //   title: 'picture'
+  // },
+  {
+    original: require('../../assets/gallery/g12-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g11-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g13-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g12-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g14-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g13-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g15-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g14-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g16-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g15-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g17-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g16-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g19-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g17-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g21-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g19-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g22-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g21-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g25-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g22-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g26-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g25-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g27-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g26-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g28-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g27-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g29-min-min.jpg'),
+    title: 'picture'
+  },
+  // {
+  //   original: require('../../assets/gallery/g30-min-min.jpg'),
+  //   title: 'picture'
+  // },
+  {
+    original: require('../../assets/gallery/g31-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g28-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g34-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g29-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g36-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g30-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g39-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g31-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g40-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g34-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g7-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g36-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g8-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g39-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g20-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g40-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g23-min-min.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g7-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/g24-min-min.jpg'),
+    title: 'picture'
+  },
+  // {
+  //   original: require('../../assets/gallery/g32-min-min.jpg'),
+  //   title: 'picture'
+  // },
+  {
+    original: require('../../assets/gallery/g33-min-min.jpg'),
+    title: 'picture'
+  },
+  // {
+  //   original: require("../../assets/gallery/g35-min-min.jpg"),
+  //   title: "picture"
+  // },
+  {
+    original: require('../../assets/gallery/1.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g8-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/2.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g20-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/3.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g23-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/4.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g24-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/5.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g32-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/6.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g33-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/7.jpg'),
+    title: 'picture'
   },
   {
-    original: require("../../assets/gallery/g35-min-min.jpg"),
-    title: "picture"
+    original: require('../../assets/gallery/8.jpg'),
+    title: 'picture'
   }
 ];
 
